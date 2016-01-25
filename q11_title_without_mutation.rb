ebe8d2bcf125c7475437a34027d6e89f7b4c2af1
Stanley Hansen <stanley321@gmail.com>
2016-01-25 15:59:21 -0800

    Q11 without mutation

---
+++ q11_title_without_mutation.rb
@@ -0,0 +1,39 @@
+  def titleize(str)
+    result = ""
+    result << str[0].upcase
+    i = 1
+    while (i < str.length) do
+      if (str[i] == ' ') then
+        # in, the, of, and, or, from
+        if (str[i+1..i+2] == "in")
+          result << " in"
+          i += 3
+        elsif (str[i+1..i+3] == "the")
+          result << " the"
+          i += 4
+        elsif (str[i+1..i+2] == "of")
+          result << " of"
+          i += 3
+        elsif (str[i+1..i+3] == "and")
+          result << " and"
+          i += 4
+        elsif (str[i+1..i+2] == "or")
+          result << " or"
+          i += 3
+        elsif (str[i+1..i+4] == "or")
+          result << " from"
+          i += 5
+        else 
+          result << " " + str[i+1].upcase
+          i += 2
+        end
+      else
+        result << str[i]
+        i += 1
+      end
+    end 
+    result
+  end
+
+string = "man of the wild"
+p titleize("man of the wild")
