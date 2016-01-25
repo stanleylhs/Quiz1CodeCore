4d60c7495e520193599cc0b860df6fad414ff2d3
Stanley Hansen <stanley321@gmail.com>
2016-01-25 13:02:28 -0800

    Q4 Prime

---
+++ q04_isPrime.js
@@ -0,0 +1,13 @@
+function isPrime(n) {
+	if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false; 
+	if (n % 2 == 0) return (n == 2);
+	var m = Math.sqrt(n);
+	for (var i = 3;i <= m;i += 2) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
+console.log("1 is a prime number. " + isPrime(1));
+console.log("5 is a prime number. " + isPrime(5));
+console.log("18 is a prime number. " + isPrime(18));
